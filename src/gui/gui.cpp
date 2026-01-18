8a38592dc3e6d9ce4e25ff1f49f6a8542d640f26
croudxd <benjamincroud@gmail.com>
2026-01-18 03:43:27 +0000

    Okay i am attempting to speed up the program and see how far we can push the simulation count, even if it might be unneeded, the current code is a a mess and it needs to be changed. Currently I migrating from a vectorized approach to a single calculation approach hopefully this will speed up our calculations as we can use the cache rather than moving these huge blocks around in memory which is just clogging up our cpu. I've started on the calculate function and got pretty far.

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <mutex>
+#include <random>
 #include <numeric>
 #include <thread>
 #include <vector>
@@ -23,6 +24,11 @@ static int simsize = 0;
 std::mutex simsize_mutex;
 
 // Results
+std::vector<float> results;
+//
+//
+//
+
 std::mutex max_result_mutex;
 static float max_result = 0.0f;
 std::mutex mean_result_mutex;
@@ -73,79 +79,43 @@ void calculate()
     std::vector<Threepoint> local_threeobjects;
     int local_simsize;
     {
-        std::lock_guard<std::mutex> lock1( objects_mutex );
-        std::lock_guard<std::mutex> lock2( threeobjects_mutex );
-        std::lock_guard<std::mutex> lock3( simsize_mutex );
+        std::lock_guard<std::mutex> lock1(objects_mutex);
+        std::lock_guard<std::mutex> lock2(threeobjects_mutex);
+        std::lock_guard<std::mutex> lock3(simsize_mutex);
         local_objects = objects;
         local_threeobjects = threeobjects;
         local_simsize = simsize;
     }
 
-    std::vector<std::vector<float>> normal_distribution_vector;
-    std::vector<std::vector<float>> triangle_distribution_vector;
+    std::vector<float> final_cogs_list;
+    final_cogs_list.reserve(local_simsize); // Allocate once!
 
-    for ( int x = 0; x < objects.size(); x++ )
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    for (int i = 0; i < local_simsize; i++) 
     {
-        normal_distribution_vector.push_back( gen_normal_distribution_vector( local_simsize, local_objects[x] ) );
+        float api_vol = local_threeobjects[10].generate(gen);
+        float rm1_qty = local_objects[0].generate(gen);
+        float rm1_price = local_threeobjects[0].generate(gen);
+
+
+
+        int step_three_batch = calc_s3_nm_batches(api_vol, local_objects[7].generate(gen));
+        int step_two_batch   = calc_s_num_batches(api_vol, local_objects[4].generate(gen), local_objects[6].generate(gen));
+        
+        float s3_t = calc_s_time(local_threeobjects[7].generate(gen), step_three_batch, local_threeobjects[8].generate(gen));
+        float s2_t = calc_s_time(local_threeobjects[5].generate(gen), step_two_batch, local_threeobjects[6].generate(gen));
+        float s1_t = calc_s_time(local_threeobjects[3].generate(gen), 0 /*batch logic*/, local_threeobjects[4].generate(gen));
+
+        float tot_time = s3_t + s2_t + s1_t + local_threeobjects[9].generate(gen);
+        
+        float rm_cost = local_objects[2].generate(gen) + (rm1_qty * rm1_price) + ...;
+        float lno_cost = (tot_time * local_threeobjects[2].generate(gen)) / api_vol;
+
+        final_cogs_list.push_back(rm_cost + lno_cost);
     }
-
-    for ( int x = 0; x < threeobjects.size(); x++ )
-    {
-        triangle_distribution_vector.push_back( gen_triangle_distribution_vector( local_simsize, local_threeobjects[x] ) );
-    }
-
-    std::vector<int> step_three_batch = s3_nm_batches(
-        triangle_distribution_vector[10], normal_distribution_vector[7] );
-    std::vector<int> step_two_batch = s_num_batches(
-        triangle_distribution_vector[10], normal_distribution_vector[4],
-        normal_distribution_vector[6] );
-    std::vector<int> step_one_batch = s_num_batches(
-        triangle_distribution_vector[10], normal_distribution_vector[3],
-        normal_distribution_vector[5] );
-
-    std::vector<float> s3 =
-        s_time( triangle_distribution_vector[7], step_three_batch,
-                triangle_distribution_vector[8] );
-    std::vector<float> s2 =
-        s_time( triangle_distribution_vector[5], step_two_batch,
-                triangle_distribution_vector[6] );
-    std::vector<float> s1 =
-        s_time( triangle_distribution_vector[3], step_one_batch,
-                triangle_distribution_vector[4] );
-
-    std::vector<float> tot_time =
-        Tot_Time( s3, s2, s1, triangle_distribution_vector[9] );
-    std::vector<float> rm =
-        RM( normal_distribution_vector[2], normal_distribution_vector[0],
-            triangle_distribution_vector[0], normal_distribution_vector[1],
-            triangle_distribution_vector[1] );
-
-    std::vector<float> lno = LnO( tot_time, triangle_distribution_vector[2],
-                                  triangle_distribution_vector[10] );
-    std::vector<float> results;
-    for ( int x = 0; x < rm.size(); x++ )
-    {
-        float result = rm[x] + lno[x];
-        results.push_back( result );
-    }
-
-    std::lock_guard<std::mutex> lock( max_result_mutex );
-    max_result = *std::max_element( results.begin(), results.end() );
-
-    std::lock_guard<std::mutex> lnopercent_lock( lnopercent_result_mutex );
-    lnopercent_result = mean( Percentage_LnO( rm, lno ) );
-
-    std::lock_guard<std::mutex> min_result_lock( min_result_mutex );
-    min_result = *std::min_element( results.begin(), results.end() );
-
-    std::lock_guard<std::mutex> mean_result_lock( mean_result_mutex );
-    mean_result = mean( results );
-    // 10th and 90th
-    std::lock_guard<std::mutex> tenthpercentile_lock( tenthpercentile_result_mutex );
-    tenthpercentile_result = percentile( results, 0.10f );
-
-    std::lock_guard<std::mutex> nintypercentile_lock( nintypercentile_result_mutex );
-    nintypercentile_result = percentile( results, 0.90f );
+    update_dashboard_results(final_cogs_list);
 }
 
 void results()
@@ -217,6 +187,7 @@ void gui( GLFWwindow* window )
             "Restart Time: Step 3",
             "Setup & Cleaning",
             "Total API Volume" };
+
         std::lock_guard<std::mutex> lock1( objects_mutex );
         for ( const auto& name : names )
             objects.emplace_back( name, 1.0f, 1.0f );
--- src/math/result.cpp
+++ src/math/result.cpp
@@ -3,88 +3,38 @@
 #include <cmath>
 #include <utility>
 
-std::vector<int> s3_nm_batches( std::vector<float> api_volume,
-                                std::vector<float> batch_size_step_three )
+int s3_nm_batches(float api_volume, float batch_size_step_three )
 {
-    std::vector<int> results;
-    results.reserve( 10000000 );
-    for ( int x = 0; x < api_volume.size(); x++ )
-    {
-        float result = round( api_volume[x] / batch_size_step_three[x] );
-        results.push_back( std::move( result ) );
-    }
-    return results;
+    float result = round( api_volume / batch_size_step_three );
+    return result;
 }
 
-std::vector<int> s_num_batches( std::vector<float> api_volume,
-                                std::vector<float> ufstep,
-                                std::vector<float> batch_size_step )
+int s_num_batches( float api_volume, float ufstep, float batch_size_step )
 {
-    std::vector<int> results;
-    results.reserve( 10000000 );
-    for ( int x = 0; x < api_volume.size(); x++ )
-    {
-        float result = round( api_volume[x] * ufstep[x] / batch_size_step[x] );
-        results.push_back( std::move( result ) );
-    }
-    return results;
+    float result = round( api_volume * ufstep / batch_size_step );
+    return result;
 }
 
-std::vector<float> s_time( std::vector<float> cycle_time_step,
-                           std::vector<int> nm_batchs,
-                           std::vector<float> restart_time_step )
+float s_time( float cycle_time_step,int nm_batchs,float restart_time_step )
 {
-    std::vector<float> results;
-    results.reserve( 10000000 );
-    for ( int x = 0; x < cycle_time_step.size(); x++ )
-    {
-        float result =
-            cycle_time_step[x] + ( nm_batchs[x] - 1.0 ) * restart_time_step[x];
-        results.push_back( std::move( result ) );
-    }
-    return results;
+    float result = cycle_time_step + ( nm_batchs - 1.0 ) * restart_time_step;
+    return result;
 }
 
-std::vector<float> Tot_Time( std::vector<float> s3_time,
-                             std::vector<float> s2_time,
-                             std::vector<float> s1_time,
-                             std::vector<float> setup_cleaning )
+float Tot_Time( float s3_time, float s2_time, float s1_time, float setup_cleaning )
 {
-    std::vector<float> results;
-    results.reserve( 10000000 );
-    for ( int x = 0; x < s3_time.size(); x++ )
-    {
-        float result = s3_time[x] + s2_time[x] + s1_time[x] + setup_cleaning[x];
-        results.push_back( std::move( result ) );
-    }
-    return results;
+    float result = s3_time + s2_time + s1_time + setup_cleaning;
+    return result;
 }
-std::vector<float> RM( std::vector<float> rm_solvntrgnt_costs,
-                       std::vector<float> rm_zts, std::vector<float> rm_zts_kg,
-                       std::vector<float> rm_zts_UF,
-                       std::vector<float> rm_zts_UF_kg )
+float RM( float rm_solvntrgnt_costs, float rm_zts, float rm_zts_kg, float rm_zts_UF, float rm_zts_UF_kg )
 {
-    std::vector<float> results;
-    results.reserve( 10000000 );
-    for ( int x = 0; x < rm_solvntrgnt_costs.size(); x++ )
-    {
-        float result = rm_solvntrgnt_costs[x] + rm_zts[x] * rm_zts_kg[x] +
-                       rm_zts_UF[x] * rm_zts_UF_kg[x];
-        results.push_back( std::move( result ) );
-    }
-    return results;
+    float result = rm_solvntrgnt_costs + rm_zts * rm_zts_kg + rm_zts_UF * rm_zts_UF_kg;
+    return result;
 }
-std::vector<float> LnO( std::vector<float> tot_time, std::vector<float> lno_rate,
-                        std::vector<float> api_volume )
+float LnO( float tot_time, float lno_rate, float api_volume )
 {
-    std::vector<float> results;
-    results.reserve( 10000000 );
-    for ( int x = 0; x < api_volume.size(); x++ )
-    {
-        float result = tot_time[x] * lno_rate[x] / api_volume[x];
-        results.push_back( std::move( result ) );
-    }
-    return results;
+    float result = tot_time * lno_rate / api_volume;
+    return result;
 }
 
 std::vector<float> Percentage_LnO( std::vector<float> rm,
